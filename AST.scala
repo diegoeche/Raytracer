9b5b8f3746c89f05ace89866fddd3f735c886199
Adolfo Builes <builes.adolfo@gmail.com>
2009-09-18 16:43:41 -0500

    Adding parser for Camera and LightSource

--- AST.scala
+++ AST.scala
@@ -5,4 +5,8 @@ sealed case class Sphere(center:Vector3d,
                          radius: Double)  extends SceneObject
 sealed case class Plane (point:Vector3d, 
                          distance: Double) extends SceneObject
+sealed case class LightSource (location: Vector3d,
+			       color: Color3f) extends SceneObject
+sealed case class Camera (location: Vector3d,
+			  lookAt: Vector3d) extends SceneObject 
 
