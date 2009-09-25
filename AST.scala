c67bf10005bc5e83435229ac911378c5b76bbae3
Adolfo Builes <builes.adolfo@gmail.com>
2009-09-24 19:51:09 -0500

    Adding changes made in the Cafeteria

--- AST.scala
+++ AST.scala
@@ -1,11 +1,15 @@
 import javax.vecmath._;
 
-sealed class SceneObject
-sealed case class Background (color : Color3f) extends SceneObject
-sealed case class Camera      (location: Vector3d , lookAt: Vector3d) extends SceneObject
-sealed case class LightSource (location: Vector3d , color: Color3f)   extends SceneObject
-sealed case class Sphere      (center:Vector3d   , radius: Double, pigment: Color3f )   extends SceneObject
-sealed case class Plane        (point:Vector3d     , distance: Double, pigment: Color3f ) extends SceneObject
+class Material(pigment: Color3f)
 
+class SceneElement
+class Geometry() 
+
+sealed case class Background  (color : Color3f)                        extends SceneElement
+sealed case class Camera      (location: Vector3d, lookAt: Vector3d)   extends SceneElement
+sealed case class LightSource (location: Vector3d, color: Color3f)     extends SceneElement
+sealed case class SceneObject (geometry: Geometry, material: Material) extends SceneElement 
+sealed case class Sphere      (center:Vector3d   , radius: Double)     extends Geometry
+sealed case class Plane	      (point:Vector3d	 , distance: Double) extends Geometry
  
 
--- Parser.scala
+++ Parser.scala
@@ -1,40 +1,77 @@
-import javax.vecmath._;
-import scala.util.parsing.combinator.syntactical._
 import java.awt.Color
+import javax.vecmath._;
+import scala.util.parsing.combinator.lexical._
+import scala.util.parsing.combinator.syntactical._
+
+/** 
+ * ExprLexical
+ * this is copied from this address. Basic support for floatTokens
+ * http://jim-mcbeath.blogspot.com/2008/09/scala-parser-combinators.html  
+ */ 
+class ExprLexical extends StdLexical {
+    override def token: Parser[Token] = floatingToken | super.token
+
+    def floatingToken: Parser[Token] =
+        rep1(digit) ~ optFraction ~ optExponent ^^
+            { case intPart ~ frac ~ exp => NumericLit(
+                    (intPart mkString "") :: frac :: exp :: Nil mkString "")}
+
+    def chr(c:Char) = elem("", ch => ch==c )
+    def sign = chr('+') | chr('-')
+    def optSign = opt(sign) ^^ {
+        case None => ""
+        case Some(sign) => sign
+    }
+    def fraction = '.' ~ rep(digit) ^^ {
+        case dot ~ ff => dot :: (ff mkString "") :: Nil mkString ""
+    }
+    def optFraction = opt(fraction) ^^ {
+        case None => ""
+        case Some(fraction) => fraction
+    }
+    def exponent = (chr('e') | chr('E')) ~ optSign ~ rep1(digit) ^^ {
+        case e ~ optSign ~ exp => e :: optSign :: (exp mkString "") :: Nil mkString ""
+    }
+    def optExponent = opt(exponent) ^^ {
+        case None => ""
+        case Some(exponent) => exponent
+    }
+}
 
 object SceneParser extends StandardTokenParsers {
-
-  lexical.delimiters ++= List("<",">",",","{","}")
+  override val lexical = new ExprLexical
+  lexical.delimiters ++= List("-","<",">",",","{","}")
   lexical.reserved ++= List( "background",
-                             "Blue",
                              "color",
                              "camera",
-                             "Green",
                              "light_source",
                              "location",
                              "look_at",
 			     "pigment",
                              "plane",
-                             "Red",
-                             "sphere",
-                             "Yellow",
-                             "White")
+                             "sphere")
+                             // "Blue",
+                             // "Green",
+                             // "Red",
+                             // "Yellow",
+                             // "White")
 
   // Doesn't accept decimals nor negatives
-  def valueP = numericLit ^^ (s => s.toDouble) 
+  def valueP = opt("-") ~ numericLit ^^ 
+    {case Some(_) ~ s => -s.toDouble
+     case None    ~ s   => s.toDouble}
   
 
   def backgroundP = "background" ~>"{" ~> colorP <~ "}" ^^
                      { case color => new Background (new Color3f (color)) }
 
 
-  def colorP  = "color" ~>
-                      ("Blue"|"Green"|"Red"|"Yellow"|"White") ^^ 
-                     {  case "Blue"   => Color.blue
-		        case "Green"  => Color.green
-		        case "Red"    => Color.red
-		        case "Yellow" => Color.yellow
-		        case "White"  => Color.white }
+  def colorP  = "color" ~> accept( "name", 
+                     {  case Identifier("Blue"  ) => Color.blue
+		        case Identifier("Green" ) => Color.green
+		        case Identifier("Red"   ) => Color.red
+		        case Identifier("Yellow") => Color.yellow
+		        case Identifier("White" ) => Color.white })
                  
   def pigmentP = "pigment" ~> "{" ~> colorP <~ "}" 
 
@@ -45,9 +82,6 @@ object SceneParser extends StandardTokenParsers {
                                                    y.toDouble, 
                                                    z.toDouble)}
 
-  
-
-
   // This one lifts a common constructor of objects like sphere and plane 
   def vectorValueP(cons: String, f: Function[(Vector3d, Double, Color3f), SceneObject]) = 
                  (cons ~> "{" ~> vectorLitP) ~ 
@@ -56,10 +90,12 @@ object SceneParser extends StandardTokenParsers {
                   {case center ~ radius ~ pigment=> f(center, radius, new Color3f ( pigment ))}
   
   def sphereP = 
-    vectorValueP("sphere", {case (center,radius,pigment) => new Sphere(center, radius, pigment)})
+    vectorValueP("sphere", {case (center,radius,pigment) => 
+      new SceneObject (new Sphere(center, radius), new Material(pigment))})
 
   def planeP = 
-    vectorValueP("plane", {case (center,radius,pigment) => new Plane(center, radius, pigment)})
+    vectorValueP("plane", {case (center,radius,pigment) => 
+      new SceneObject (new Plane(center, radius), new Material(pigment))})
   
   def cameraP = ("camera" ~> "{" ~> "location"~> vectorLitP ) ~
                 ( "look_at" ~> vectorLitP <~ "}") ^^
@@ -72,17 +108,17 @@ object SceneParser extends StandardTokenParsers {
   def sceneObjP = sphereP | planeP | cameraP | lightP | backgroundP
 
   // The scene is just a list of SceneObjects
-  def sceneP: Parser[List[SceneObject]] = rep(sceneObjP)
+  def sceneP: Parser[List[SceneElement]] = rep(sceneObjP)
 
   def parse(s:String) = {
     val tokens = new lexical.Scanner(s)
     // Check there's only one camera and LightSource.
-    def checkTree (tree:List[SceneObject]) = (tree count (_.isInstanceOf[Camera])) >=0   &&
-                                                                    (tree count (_.isInstanceOf[LightSource])) >= 0
+     // def checkTree (tree:List[SceneObject]) = (tree count (_.isInstanceOf[Camera])) >=0   &&
+     //                                         (tree count (_.isInstanceOf[LightSource])) >= 0
     // lastFailure = None
     phrase(sceneP)(tokens) match {
       case Success(tree,_) => 
-        if (checkTree(tree)) {
+        if (true) { // checkTree(tree)
           Right(tree)
         }else{
           Left("Error in the number of cameras or light sources.")
