8ab710f6adafb31eb2c2a9e0aa07d22f6a3f4cc8
Diego Echeverri <diegoeche@gmail.com>
2009-09-28 20:59:34 -0500

    added template for file processing

--- SceneCreator.scala
+++ SceneCreator.scala
@@ -10,7 +10,7 @@ import javax.media.j3d._;
 import javax.vecmath._;
 import com.sun.j3d.utils.applet.MainFrame; 
 import com.sun.j3d.utils.universe._; 
-import com.sun.j3d.utils.geometry._; 
+import com.sun.j3d.utils._; 
 
 object Main extends Application {
   val applet = new Applet() {
@@ -20,10 +20,42 @@ object Main extends Application {
     val canvas3D = new Canvas3D(config);
     add("Center", canvas3D);
     val scene = createSceneGraph();
+    parseScene();
     val simpleU = new SimpleUniverse(canvas3D);
+
     simpleU.getViewingPlatform().setNominalViewingTransform();
     simpleU.addBranchGraph(scene);
-    
+
+    def process(l: List[SceneElement]) = {
+      l.foreach {
+        case Background(c)    => ()  // TODO
+        case LightSource(l,c) => ()  // TODO
+        case Camera(l,la)     => ()  // TODO
+        case SceneObject(g,m) => 
+          val tg = new TransformGroup()
+          val pos = new Transform3D()
+          g match {
+            case Sphere(c, r) => {
+              val sphere = new geometry.Sphere(r.toFloat)
+              pos.set(c)
+              tg.setTransform(pos)
+              tg.addChild(sphere)
+              scene.addChild(tg)
+            }
+        }
+
+        case _                => ()
+      }
+    }
+
+    def parseScene() = {
+      val text = io.Source.fromPath("scene.txt").mkString
+      SceneParser.parse(text) match {
+        case Left(err) => {println(err); exit(0);}
+        case Right(tree) => process(tree);
+      }
+    }
+
     def createSceneGraph(): BranchGroup = {
       val objRoot = new BranchGroup();
       // Lights
@@ -44,7 +76,5 @@ object Main extends Application {
       return objRoot;
     } 
   }
-  
-
   new MainFrame(applet, 256, 256);
 }
